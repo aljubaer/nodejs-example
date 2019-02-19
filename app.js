b8dd3620526a27598c5e6b9d3f6b487b1f30e71d
Abdullah Al Jubaer <bsse0812@iit.du.ac.bd>
2019-02-20 02:20:11 +0600

    Error handling, CORS check

--- app.js
+++ app.js
@@ -2,9 +2,49 @@
 
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productsRoute = require('./api/routes/products');
+const ordersRoute = require('./api/routes/orders');
+
+// Logging all request in console using morgan
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Origin',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 
 app.use('/products', productsRoute);
+app.use('/orders', ordersRoute);
+
+// Errors will caught here
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status(404);
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
